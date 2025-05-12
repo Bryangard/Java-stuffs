0a46b78f9736edf17880444cb84041ab8048d86a
bryangard <bryanworkyee@gmail.com>
2025-05-12 15:35:32 +0800

    simple java if else program

--- javaifelse.java
+++ javaifelse.java
@@ -1,5 +1,21 @@
-public class javaifelse {
+import java.util.Scanner;
+
+class javaifelse {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int num1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number that is less than 10: ");
+        num1 = scanner.nextInt();
+
+        if (num1 < 10) {
+            System.out.println("the number is less than 10");
+        } else {
+            System.out.println("You input a number that is greater than 10");
+        }
+
+        scanner.close();
+
     }
 }
