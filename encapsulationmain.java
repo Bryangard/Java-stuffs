f1bc4782eeeaa00a8e30fce149589450c63e033d
bryangard <bryanworkyee@gmail.com>
2025-05-13 13:19:26 +0800

    encapsulation main

---
+++ encapsulationmain.java
@@ -0,0 +1,14 @@
+public class encapsulationmain {
+
+    public static void main(String[] args) {
+
+        encapsulationstudent s = new encapsulationstudent();
+        s.setName("John Doe");
+        System.out.println(s.getName());
+
+        s.setAge(21);
+        System.out.println(s.getAge());
+
+    }
+
+}
---
+++ encapsulationstudent.java
@@ -0,0 +1,25 @@
+public class encapsulationstudent {
+
+    private String name;
+    private int age;
+
+    public void setName(String newName) {
+
+        name = newName;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setAge(int newAge) {
+
+        age = newAge;
+    }
+
+    public int getAge() {
+
+        return age;
+    }
+}
